33610e441f26158ad99bd21be9844dc85b3ac498
Zakhar Urvantsev <zurvantsev@ozon.ru>
2023-03-26 22:06:41 +0300

    lab5 is done

---
+++ src/main/java/com/example/backend/controllers/CountryController.java
@@ -0,0 +1,83 @@
+package com.example.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import com.example.backend.models.Country;
+import com.example.backend.repositories.CountryRepository;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CountryController {
+    @Autowired
+    CountryRepository countryRepository;
+
+    @GetMapping("/countries")
+    public List
+    getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @PostMapping("/countries")
+    public ResponseEntity<Object> createCountry(@RequestBody Country country)
+            throws Exception {
+        try {
+            Country nc = countryRepository.save(country);
+            return new ResponseEntity<Object>(nc, HttpStatus.OK);
+        }
+        catch(Exception ex) {
+            String error;
+            if (ex.getMessage().contains("countries.name_UNIQUE"))
+                error = "countyalreadyexists";
+            else
+                error = "undefinederror";
+            Map<String, String>
+                    map =  new HashMap<>();
+            map.put("error", error);
+            return new ResponseEntity<Object> (map, HttpStatus.OK);
+        }
+    }
+
+
+    @PutMapping("/countries/{id}")
+    public ResponseEntity<Country> updateCountry(@PathVariable(value = "id") Long countryId,
+                                                 @RequestBody Country countryDetails) {
+        Country country = null;
+        Optional<Country>
+                cc = countryRepository.findById(countryId);
+        if (cc.isPresent()) {
+            country = cc.get();
+            country.name = countryDetails.name;
+            countryRepository.save(country);
+            return ResponseEntity.ok(country);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "country not found");
+        }
+    }
+
+    @DeleteMapping("/countries/{id}")
+    public ResponseEntity<Object> deleteCountry(@PathVariable(value = "id") Long countryId) {
+        Optional<Country>
+                country = countryRepository.findById(countryId);
+        Map<String, Boolean>
+                resp = new HashMap<>();
+        if (country.isPresent()) {
+            countryRepository.delete(country.get());
+            resp.put("deleted", Boolean.TRUE);
+        }
+        else
+            resp.put("deleted", Boolean.FALSE);
+        return ResponseEntity.ok(resp);
+    }
+}
