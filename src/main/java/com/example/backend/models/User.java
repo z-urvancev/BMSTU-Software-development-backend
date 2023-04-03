7b532a35231b9c53a6e572c5f8166861d51b26ef
Zakhar Urvantsev <zurvantsev@ozon.ru>
2023-04-03 23:14:21 +0300

    lab5

---
+++ src/main/java/com/example/backend/models/User.java
@@ -0,0 +1,52 @@
+package com.example.backend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Access(AccessType.FIELD)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    public long id;
+    @Column(name = "email", nullable = false, unique = true)
+    public String email;
+
+    @Column(name = "login", nullable = false, unique = true)
+    public String login;
+
+    @JsonIgnore
+    @Column(name = "password")
+    public String password;
+
+    @JsonIgnore
+    @Column(name = "salt")
+    public String salt;
+
+    @Column(name = "token")
+    public String token;
+
+    @Column(name = "activity")
+    public LocalDateTime activity;
+
+    @ManyToMany(mappedBy = "users")
+    public Set<Museum> museums = new HashSet<Museum>();
+
+
+    public void addMuseum(Museum m) {
+        this.museums.add(m);
+        m.users.add(this);
+    }
+
+    public void removeMuseum(Museum m) {
+        this.museums.remove(m);
+        m.users.remove(this);
+    }
+}
