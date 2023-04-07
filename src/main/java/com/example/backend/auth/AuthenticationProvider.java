9e8f3b5c99e1b8b5a92e86224a18e645a3993f88
Zakhar Urvantsev <zurvantsev@ozon.ru>
2023-04-07 14:42:19 +0300

    LAB7

---
+++ src/main/java/com/example/backend/auth/AuthenticationProvider.java
@@ -0,0 +1,47 @@
+package com.example.backend.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import com.example.backend.repositories.UserRepository;
+
+import java.util.Optional;
+
+@Component
+public class AuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails,
+                                                  UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String userName,
+                                       UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
+
+        Object token = usernamePasswordAuthenticationToken.getCredentials();
+        Optional<com.example.backend.models.User>
+                uu = userRepository.findByToken(String.valueOf(token));
+        if (!uu.isPresent())
+            throw new UsernameNotFoundException("user is not found");
+        com.example.backend.models.User u = uu.get();
+
+        UserDetails user= new User(u.login, u.password,
+                true,
+                true,
+                true,
+                true,
+                AuthorityUtils.createAuthorityList("USER"));
+        return user;
+    }
+}
--- src/main/java/com/example/backend/repositories/UserRepository.java
+++ src/main/java/com/example/backend/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.backend.models.User;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long>
 {
 
+    Optional<User> findByToken(String valueOf);
 }
