7832b85c561518759ae9cc5adbf271a67716326d
Zakhar Urvancev <92048210+z-urvancev@users.noreply.github.com>
2023-05-07 21:06:25 +0300

    Lab10 (#8)
    * lab9
    
    * lab 10 initial
    
    ---------
    
    Co-authored-by: Zakhar Urvantsev <zurvantsev@ozon.ru>

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -4,13 +4,16 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 import {Link, useNavigate} from 'react-router-dom';
 import Utils from "../utils/Utils";
+import {connect} from 'react-redux';
 import BackendService from "../services/BackendService";
+import {userActions} from "../utils/Rdx";
 
 class NavigationBarClass extends React.Component {
 
     constructor(props) {
         super(props);
         this.goHome = this.goHome.bind(this);
+        this.logout = this.logout.bind(this);
     }
 
     goHome() {
@@ -18,22 +21,27 @@ class NavigationBarClass extends React.Component {
     }
 
     logout() {
-        BackendService.logout().then(() => {
-            Utils.removeUser();
-            this.goHome()
-        });
+        BackendService.logout()
+            .then(() => {
+                Utils.removeUser();
+                this.props.dispatch(userActions.logout())
+                this.props.navigate('Login');
+            })
     }
 
     render() {
         let uname = Utils.getUserName();
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand><FontAwesomeIcon icon={faHome}/>{' '}My RPO</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
+                <Navbar.Brand>myRPO</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto">
+                        {/*<Nav.Link href="/home">Home</Nav.Link>*/}
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link onClick={this.goHome}>Another Home</Nav.Link>
+                        <Nav.Link onClick={this.goHome}>Another home</Nav.Link>
+                        <Nav.Link onClick={() => { this.props.history.push("/home")}} >Yet another home</Nav.Link>
                     </Nav>
                     <Navbar.Text className='me-2'>{uname}</Navbar.Text>
                     {uname && <Nav.Link className="me-2" onClick={this.logout}><FontAwesomeIcon icon={faUser}
@@ -41,6 +49,13 @@ class NavigationBarClass extends React.Component {
                     {!uname && <Nav.Link className="me-2" as={Link} to="/login"><FontAwesomeIcon icon={faUser}
                                                                                                  fixedWidth/>{' '}Вход</Nav.Link>}
                 </Navbar.Collapse>
+                <Navbar.Text>{this.props.user && this.props.user.login}</Navbar.Text>
+                { this.props.user &&
+                    <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
+                }
+                { !this.props.user &&
+                    <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
+                }
             </Navbar>
         );
     }
@@ -52,4 +67,9 @@ const NavigationBar = props => {
     return <NavigationBarClass navigate={navigate} {...props} />
 }
 
-export default NavigationBar;
+const mapStateToProps = state => {
+    const { user } = state.authentication;
+    return { user };
+}
+
+export default  connect(mapStateToProps)(NavigationBar);
--- frontend/src/services/BackendService.jsx
+++ frontend/src/services/BackendService.jsx
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import Utils from "../utils/Utils";
+import {alertActions, store} from "../utils/Rdx";
 
 const API_URL = 'http://localhost:8081/api/v1'
 const AUTH_URL = 'http://localhost:8081/auth'
@@ -10,8 +12,37 @@ class BackendService {
     }
 
     logout() {
-        return axios.get(`${AUTH_URL}/logout`)
+        return axios.get(`${AUTH_URL}/logout`, {headers : {Authorization :
+                    Utils.getToken()}})
     }
 }
 
+function showError(msg)
+{
+    store.dispatch(alertActions.error(msg))
+}
+
+axios.interceptors.request.use(
+    config => {
+        store.dispatch(alertActions.clear())
+        let token = Utils.getToken();
+        if (token)
+            config.headers.Authorization = token;
+        return config;
+    },
+    error => {
+        showError(error.message)
+        return Promise.reject(error);
+    })
+
+axios.interceptors.response.use(undefined,
+    error => {
+        if (error.response && error.response.status && [401, 403].indexOf(error.response.status) !== -1)
+            showError("Ошибка авторизации")
+        else if (error.response && error.response.data && error.response.data.message)
+            showError(error.response.data.message)
+        else
+            showError(error.message)
+        return Promise.reject(error);
+    })
 export default new BackendService()
